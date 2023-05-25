f5a179cddc98c2be897414901fbd8dffa547c14c
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-25 19:34:17 -0300

    adding html document

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Welcome to space!</p>')
-
+    return render(request, 'gallery/index.html')
